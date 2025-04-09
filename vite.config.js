3af4a0f482cbf99c7a6c19f38cbed34a85406181
aryamadhavi03 <151993133+aryamadhavi03@users.noreply.github.com>
2025-04-09 08:53:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/githubs_pages/",
+  base: "/PWA/",
   server: {
     headers: {
       "Permissions-Policy": "interest-cohort=()"
